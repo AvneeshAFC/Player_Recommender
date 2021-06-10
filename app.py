238df1b6df41a20e6d7c1bba76f5bbb6909999b1
Avneesh Saini <avneeshsinghsaini@gmail.com>
2021-06-11 02:16:43 +0530

    added preferred foot filter

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ with params:
     st.text(' \n')
     st.header('Tweak the parameters')
     
-    col1, col2 = st.beta_columns([1, 2])
+    col1, col2, col3 = st.beta_columns([1, 2.2, 0.8])
     with col1:
         radio = st.radio('Player type', ['Outfield players', 'Goal Keepers'])    
     with col2:
@@ -69,22 +69,27 @@ with params:
         age_default = (min(df['Age']), max(df['Age']))
         query = st.selectbox('Player name', players, 
             help='Type without deleting a character. To search from a specific team, just type in the club\'s name.')
-
-    col3, col4, col5, col6 = st.beta_columns([0.7, 1, 1, 1])
     with col3:
+        foot = st.selectbox('Preferred foot', ['All', 'Automatic', 'Right', 'Left'], 
+        help='\'Automatic\' matches the preferred foot of the selected player with the players automatically. \
+            \'All\' by default. Preferred foot data is not available for GK\'s.')
+    
+
+    col4, col5, col6, col7 = st.beta_columns([0.7, 1, 1, 1])
+    with col4:
         if radio=='Outfield players':
             res, val, step = (5, 20), 10, 5
         else:
             res, val, step = (3, 10), 5, 1
         count = st.slider('Number of results', min_value=res[0], max_value=res[1], value=val, step=step)
-    with col4:
+    with col5:
         comp = st.selectbox('League', ['All', 'Premier League', 'La Liga', 'Serie A', 'Bundesliga', 'Ligue 1'],
             help='Leagues to get recommendations from. \'All\' leagues by default.')
-    with col5:
+    with col6:
         comparison = st.selectbox('Comparison with', ['All positions', 'Same position'],
             help='Whether to compare the selected player with all positions or just the same defined position in the dataset. \'All \
             positions\' by default.')
-    with col6:
+    with col7:
         age = st.slider('Age bracket', min_value=age_default[0], max_value=age_default[1], value=age_default, 
         help='Age range to get recommendations from. Drag the sliders on either side. \'All\' ages by default.')
     
@@ -97,8 +102,11 @@ with result:
     st.markdown('_showing recommendations for_ **{}**'.format(query))
     
 
-    def getRecommendations(metric, league='All', comparison='All positions', age=age_default, count=val):
-        df_res = df.iloc[:, [1, 3, 5, 6, 11]].copy()
+    def getRecommendations(metric, df_type, league='All', foot='All', comparison='All positions', age=age_default, count=val):
+        if df_type == 'outfield':
+            df_res = df.iloc[:, [1, 3, 5, 6, 11, -1]].copy()
+        else:
+            df_res = df.iloc[:, [1, 3, 5, 6, 11]].copy()
         df_res['Player'] = list(player_ID.keys())
         df_res.insert(1, 'Similarity', metric)
         df_res = df_res.sort_values(by=['Similarity'], ascending=False)
@@ -107,7 +115,7 @@ with result:
         df_res = df_res.iloc[1:, :]
 
         
-        if comparison == 'Same position':
+        if comparison == 'Same position' and df_type == 'outfield':
             q_pos = list(df[df['Player']==query.split(' (')[0]].Pos)[0]
             df_res = df_res[df_res['Pos']==q_pos]
 
@@ -123,6 +131,17 @@ with result:
         else:
             df_res = df_res[(df_res['Age'] >= age[0]) & (df_res['Age'] <= age[1])]
         
+
+        if foot=='All' or df_type == 'gk':
+            pass
+        elif foot=='Automatic':
+            query_foot = df['Foot'][player_ID[query]]
+            df_res = df_res[df_res['Foot']==query_foot]
+        elif foot=='Left':
+            df_res = df_res[df_res['Foot']=='left']
+        else:
+            df_res = df_res[df_res['Foot']=='right']
+        
         
         df_res = df_res.iloc[:count, :].reset_index(drop=True)
         df_res.index = df_res.index + 1
@@ -134,5 +153,6 @@ with result:
 
 
     sims = engine[query]
-    recoms = getRecommendations(sims, league=comp, comparison=comparison, age=age, count=count)
+    df_type = 'outfield' if len(df) == 2040 else 'gk'
+    recoms = getRecommendations(sims, df_type=df_type, foot=foot, league=comp, comparison=comparison, age=age, count=count)
     st.table(recoms)
